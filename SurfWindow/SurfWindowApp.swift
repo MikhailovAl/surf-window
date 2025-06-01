a8f487a877450c9e6ea74ad301a70d366b446b89
mikhailov_al <alexei.mikhailov@lionsoul.com>
2025-06-01 16:12:15 +0200

    Initial Commit

---
+++ SurfWindow/SurfWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfWindowApp.swift
+//  SurfWindow
+//
+//  Created by Mikhailov Alexei on 01.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SurfWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
